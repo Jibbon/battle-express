4eb4ce0491d5a4f934e0c8a297d31a8baa52870f
Jibbon <joseph@jibbon.com>
2020-09-10 20:06:35 +1000

    setup

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send({
-    "Output": "Hello Worlds!"
-  });
-});
-
-app.post('/', function(req, res) {
-  res.send({
-    "Output": "Hello Worldsss!"
-  });
-});
-
+app.use(express.static('public', {
+  maxAge: '31556952000' // uses milliseconds per docs
+}))
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
